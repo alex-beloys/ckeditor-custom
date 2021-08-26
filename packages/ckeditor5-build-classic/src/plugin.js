40e16f323fdeaeceef9ea7c1b66198cfe558392d
Александр Белоус <venseon@Aleksandrs-MacBook-Air.local>
2021-08-26 09:27:55 +0300

    Modified plugin

--- packages/ckeditor5-build-classic/src/plugin.js
+++ packages/ckeditor5-build-classic/src/plugin.js
@@ -9,7 +9,7 @@ export default class HCardEditing extends Plugin {
   }
 
   static get pluginName() {
-	return 'Slot alias items'
+	return 'Test plugin'
   }
 
   init() {
@@ -17,8 +17,6 @@ export default class HCardEditing extends Plugin {
 	this._defineConverters();
 	this._defineClipboardInputOutput();
 
-	// View-to-model position mapping is needed because an h-card element in the model is represented by a single element,
-	// but in the view it is a more complex structure.
 	this.editor.editing.mapper.on(
 	  'viewToModelPosition',
 	  viewToModelPositionOutsideModelElement( this.editor.model, viewElement => viewElement.hasClass( 'slot-item' ) )
@@ -37,54 +35,44 @@ export default class HCardEditing extends Plugin {
   _defineConverters() {
 	const conversion = this.editor.conversion;
 
-	// Data-to-model conversion.
 	conversion.for( 'upcast' ).elementToElement( {
 	  view: {
 		name: 'span',
 		classes: [ 'slot-item' ]
 	  },
 	  model: ( viewElement, { writer } ) => {
-		return writer.createElement( 'h-card', getCardDataFromViewElement( viewElement ) );
+		return writer.createElement( 'slot-item', getCardDataFromViewElement( viewElement ) );
 	  }
 	} );
 
-	// Model-to-data conversion.
 	conversion.for( 'dataDowncast' ).elementToElement( {
 	  model: 'slot-item',
 	  view: ( modelItem, { writer: viewWriter } ) => createCardView( modelItem, viewWriter )
 	} );
 
-	// Model-to-view conversion.
 	conversion.for( 'editingDowncast' ).elementToElement( {
 	  model: 'slot-item',
 	  view: ( modelItem, { writer: viewWriter } ) => toWidget( createCardView( modelItem, viewWriter ), viewWriter )
 	} );
 
-	// Helper method for both downcast converters.
 	function createCardView( modelItem, viewWriter ) {
-	  const alias = modelItem.getAttribute( 'alias' ); //
+	  const alias = modelItem.getAttribute( 'alias' );
 
 	  const cardView = viewWriter.createContainerElement( 'span', { class: 'slot-item' } );
+	  const phoneView = viewWriter.createContainerElement( 'span', { class: 'p-alias' } );
 
-	  const aliasView = viewWriter.createContainerElement( 'span', { class: 'slot-alias' }) //
-
-	  viewWriter.insert( viewWriter.createPositionAt( aliasView, 0 ), viewWriter.createText( alias ) );
-
-	  viewWriter.insert( viewWriter.createPositionAt( cardView, 0 ), aliasView );
+	  viewWriter.insert( viewWriter.createPositionAt( phoneView, 0 ), viewWriter.createText( alias ) );
+	  viewWriter.insert( viewWriter.createPositionAt( cardView, 0 ), phoneView );
 
 	  return cardView;
 	}
   }
 
-  // Integration with the clipboard pipeline.
   _defineClipboardInputOutput() {
 	const view = this.editor.editing.view;
 	const viewDocument = view.document;
 
-	// Processing pasted or dropped content.
 	this.listenTo( viewDocument, 'clipboardInput', ( evt, data ) => {
-	  // The clipboard content was already processed by the listener on the higher priority
-	  // (for example while pasting into the code block).
 	  if ( data.content ) {
 		return;
 	  }
@@ -95,25 +83,21 @@ export default class HCardEditing extends Plugin {
 		return;
 	  }
 
-	  // Use JSON data encoded in the DataTransfer.
 	  const slot = JSON.parse( slotData );
 
-	  // Translate the h-card data to a view fragment.
 	  const writer = new UpcastWriter( viewDocument );
 	  const fragment = writer.createDocumentFragment();
 
 	  writer.appendChild(
-		writer.createElement( 'span', { class: 'h-card' }, [
-		  writer.createElement( 'span', { class: 'slot-alias' }, slot.alias )
+		writer.createElement( 'span', { class: 'slot-item' }, [
+		  writer.createElement( 'span', { class: 'p-alias' }, slot.alias )
 		] ),
 		fragment
 	  );
 
-	  // Provide the content to the clipboard pipeline for further processing.
 	  data.content = fragment;
 	} );
 
-	// Processing copied, pasted or dragged content.
 	this.listenTo( document, 'clipboardOutput', ( evt, data ) => {
 	  if ( data.content.childCount != 1 ) {
 		return;
@@ -128,16 +112,12 @@ export default class HCardEditing extends Plugin {
   }
 }
 
-//
-// H-Card helper functions.
-//
-
 function getCardDataFromViewElement( viewElement ) {
   const children = Array.from( viewElement.getChildren() );
-  const aliasElement = children.find( element => element.is( 'element', 'span') && element.hasClass('slot-alias'))
+  const aliasElement = children.find( element => element.is( 'element', 'span' ) && element.hasClass( 'p-alias' ) );
 
   return {
-	alias: getText(aliasElement),
+	alias: getText( aliasElement ),
   };
 }
 
