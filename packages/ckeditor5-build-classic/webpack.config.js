c9b4827918592be9f3ff80e835cf8cef5eae0180
Александр Белоус <venseon@Aleksandrs-MacBook-Air.local>
2021-09-02 13:48:48 +0300

    Extract css

--- packages/ckeditor5-build-classic/webpack.config.js
+++ packages/ckeditor5-build-classic/webpack.config.js
@@ -12,6 +12,7 @@ const webpack = require( 'webpack' );
 const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
 	devtool: 'source-map',
@@ -54,7 +55,10 @@ module.exports = {
 		new webpack.BannerPlugin( {
 			banner: bundler.getLicenseBanner(),
 			raw: true
-		} )
+		} ),
+	  new MiniCssExtractPlugin( {
+			filename: 'styles.css'
+	  } )
 	],
 
 	module: {
@@ -66,15 +70,8 @@ module.exports = {
 			{
 				test: /\.css$/,
 				use: [
-					{
-						loader: 'style-loader',
-						options: {
-							injectType: 'singletonStyleTag',
-							attributes: {
-								'data-cke': true
-							}
-						}
-					},
+				  MiniCssExtractPlugin.loader,
+				  'css-loader',
 					{
 						loader: 'postcss-loader',
 						options: styles.getPostCssConfig( {
